5cdc965af32f55555ceab5fe4f9583b4c9c4abae
Tatiana-adv <arlualimi@gmail.com>
2025-09-09 09:54:49 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Este é um arquivo de exemplo para compressão
+(function() {
+  const saudacao = "Olá, Grunt!";
+  console.log(saudacao);
+  
+  function mostrarAlerta() {
+    alert("Código JavaScript comprimido!");
+  }
+
+  // Mostra um alerta após 3 segundos
+  setTimeout(mostrarAlerta, 3000);
+})();
